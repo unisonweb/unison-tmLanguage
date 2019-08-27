41e1d33aadcac9886612e566871dbf4a74a803da
Ben Fradet <benjamin.fradet@gmail.com>
2019-08-27 22:24:19 +0200

    Add gulp file

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+const gulp = require("gulp");
+const mocha = require("gulp-mocha");
+const json2cson = require("gulp-json2cson");
+const ts = require("gulp-typescript");
+const plist = require("plist");
+const fs = require("fs");
+const path = require("path");
+
+const inputGrammar = "src/unison.tmLanguage.json";
+const grammarsDirectory = "grammars/";
+const jsOut = "out/";
+
+function handleError(err) {
+    console.log(err.toString());
+    process.exit(-1);
+}
+
+gulp.task('buildTmLanguage', done => {
+    const text = fs.readFileSync(inputGrammar);
+    const jsonData = JSON.parse(text);
+    const plistData = plist.build(jsonData);
+
+    if (!fs.existsSync(grammarsDirectory)) {
+        fs.mkdirSync(grammarsDirectory);
+    }
+
+    fs.writeFileSync(path.join(grammarsDirectory, 'unison.tmLanguage'), plistData);
+
+    done();
+});
+
+gulp.task('buildAtom', () => {
+    return gulp.src(inputGrammar)
+        .pipe(json2cson())
+        .pipe(gulp.dest(grammarsDirectory))
+        .on("error", handleError);
+});
+
+gulp.task('compile', () => {
+    const tsProject = ts.createProject("./tsconfig.json");
+    return tsProject.src()
+        .pipe(tsProject())
+        .pipe(gulp.dest(jsOut));
+});
+
+gulp.task('test', gulp.series('compile', done => {
+    const result = gulp.src(jsOut + "test/**/*.tests.js")
+        .pipe(mocha())
+        .on("error", handleError);
+
+    done();
+
+    return result;
+}));
+
+gulp.task('default',
+    gulp.series(
+        gulp.parallel('buildAtom', 'buildTmLanguage'),
+        'test'));
