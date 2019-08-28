573335e8cc0638548dec7150b686a0ec07fd3014
Ben Fradet <benjamin.fradet@gmail.com>
2019-08-28 21:41:04 +0200

    Add comment tokens

--- tests/utils/tokenize.ts
+++ tests/utils/tokenize.ts
@@ -1,4 +1,4 @@
-import { ITokenizeLineResult, Registry, StackElement } from 'vscode-textmate';
+// heavily inspired by https://github.com/dotnet/csharp-tmLanguage/blob/master/test/utils/tokenize.ts
 
 export interface Token {
   text: string;
@@ -7,4 +7,13 @@ export interface Token {
 
 function createToken(text: string, type: string) {
   return { text, type };
+}
+
+export namespace Token {
+  export namespace Comment {
+    export namespace SingleLine {
+      export const Start = createToken('--', 'punctuation.definition.comment.unison')
+      export const Text = (text: string) => createToken(text, 'comment.line.double-dash.unison')
+    }
+  }
 }
