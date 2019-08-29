7ce764739f69e7131150aef4adf88fa853500c0e
Ben Fradet <benjamin.fradet@gmail.com>
2019-08-29 20:21:54 +0200

    Add Input model

--- tests/utils/tokenize.ts
+++ tests/utils/tokenize.ts
@@ -1,4 +1,4 @@
-// heavily inspired by https://github.com/dotnet/csharp-tmLanguage/blob/master/test/utils/tokenize.ts
+// adapted from https://github.com/dotnet/csharp-tmLanguage/blob/master/test/utils/tokenize.ts
 
 export interface Token {
   text: string;
@@ -15,5 +15,30 @@ export namespace Token {
       export const Start = createToken('--', 'punctuation.definition.comment.unison')
       export const Text = (text: string) => createToken(text, 'comment.line.double-dash.unison')
     }
+  }
+}
+
+interface Span {
+  startLine: number;
+  startIndex: number;
+  endLine: number;
+  endIndex: number;
+}
+
+export class Input {
+  private constructor(
+    public lines: string[],
+    public span: Span
+  ) {}
+
+  public static FromText(text: string) {
+    text = text.replace('\r\n', '\n');
+    let lines = text.split('\n');
+    return new Input(lines, {
+      startLine: 0,
+      startIndex: 0,
+      endLine: lines.length - 1,
+      endIndex: lines[lines.length - 1].length
+    })
   }
 }
