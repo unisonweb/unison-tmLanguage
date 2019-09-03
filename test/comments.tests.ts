cbfce2692ed840ea32333df23a865e4b1dac55b9
Ben Fradet <benjamin.fradet@gmail.com>
2019-09-03 17:37:11 +0200

    Test eof comments

--- test/comments.tests.ts
+++ test/comments.tests.ts
@@ -27,12 +27,63 @@ describe("Grammar", () => {
     });
 
     it("single-line double comment", () => {
-        const input = `-- -- foo`;
-        const tokens = tokenize(input);
+      const input = `-- -- foo`;
+      const tokens = tokenize(input);
 
-        tokens.should.deep.equal([
-            Token.Comment.SingleLine.Start,
-            Token.Comment.SingleLine.Text(" -- foo")]);
+      tokens.should.deep.equal([
+        Token.Comment.SingleLine.Start,
+        Token.Comment.SingleLine.Text(" -- foo")
+      ]);
+    });
+
+    it("eof comment spanning multiple lines", () => {
+      const input = `--- foo
+bar
+baz
+`;
+      const tokens = tokenize(input);
+
+      tokens.should.deep.equal([
+        Token.Comment.Eof.Start,
+        Token.Comment.Eof.Text(" foo"),
+        Token.Comment.Eof.Text("bar"),
+        Token.Comment.Eof.Text("baz")
+      ]);
+    });
+
+    it("eof comment spanning one line", () => {
+      const input = `--- foo
+`;
+      const tokens = tokenize(input);
+
+      tokens.should.deep.equal([
+        Token.Comment.Eof.Start,
+        Token.Comment.Eof.Text(" foo")
+      ]);
+    });
+
+    it("eof comment after something", () => {
+      const input = `square x = x * x --- foo
+`;
+      const tokens = tokenize(input);
+
+      tokens.should.deep.equal([
+        Token.Source.Text("square x = x * x "),
+        Token.Comment.Eof.Start,
+        Token.Comment.Eof.Text(" foo")
+      ]);
+    });
+
+    it("eof comment after whitespace", () => {
+      const input = `  --- foo
+`;
+      const tokens = tokenize(input);
+
+      tokens.should.deep.equal([
+        Token.Comment.LeadingWhitespace("  "),
+        Token.Comment.Eof.Start,
+        Token.Comment.Eof.Text(" foo")
+      ]);
     });
   });
 });
