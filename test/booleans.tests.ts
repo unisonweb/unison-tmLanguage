aa3673fe594b5bf109c7a8bd7b037d5a834d92cb
Ben Fradet <benjamin.fradet@gmail.com>
2019-09-04 15:42:49 +0200

    Add tests for the boolean grammar

---
+++ test/booleans.tests.ts
@@ -0,0 +1,22 @@
+import { should } from 'chai';
+import { tokenize, Input, Token } from './utils/tokenize';
+
+describe("Grammar", () => {
+  before(() => { should(); });
+
+  describe("Booleans", () => {
+    it("true", () => {
+      const input = `true`;
+      const tokens = tokenize(input)
+
+      tokens.should.deep.equal([Token.Boolean.True]);
+    });
+
+    it("false", () => {
+      const input = `false`;
+      const tokens = tokenize(input)
+
+      tokens.should.deep.equal([Token.Boolean.False]);
+    });
+  });
+});
