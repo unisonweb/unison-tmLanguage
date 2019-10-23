99267675b00cfb7f745fb4abb5d606e9aa53805a
Ben Fradet <benjamin.fradet@gmail.com>
2019-10-23 16:34:22 +0200

    Test strings containing escaped double quotes

--- test/strings.tests.ts
+++ test/strings.tests.ts
@@ -15,5 +15,19 @@ describe("Grammar", () => {
         Token.Text.End
       ]);
     });
+
+    it("escaped", () => {
+      const input = `"hello \\"world\\""`;
+      const tokens = tokenize(input)
+
+      tokens.should.deep.equal([
+        Token.Text.Start,
+        Token.Text.String("hello "),
+        Token.Text.CharacterEscape,
+        Token.Text.String("world"),
+        Token.Text.CharacterEscape,
+        Token.Text.End
+      ]);
+    });
   });
 });
