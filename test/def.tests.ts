66beb72fdb76445af885e3c56ad794143fb3964e
Ben Fradet <benjamin.fradet@gmail.com>
2019-11-29 16:20:06 +0100

    Test function definition highlighting

---
+++ test/def.tests.ts
@@ -0,0 +1,29 @@
+import { should } from 'chai';
+import { tokenize, Input, Token } from './utils/tokenize';
+
+describe("Grammar", () => {
+  before(() => { should(); });
+
+  describe("Function definitions", () => {
+    it("with space", () => {
+      const input = `timesTwo :`;
+      const tokens = tokenize(input)
+
+      tokens.should.deep.equal([
+        Token.Def.Fn("timesTwo"),
+        Token.Source.Text(" "),
+        Token.Def.Colon
+      ]);
+    });
+
+    it("without space", () => {
+      const input = `timesTwo:`;
+      const tokens = tokenize(input)
+
+      tokens.should.deep.equal([
+        Token.Def.Fn("timesTwo"),
+        Token.Def.Colon
+      ]);
+    });
+  });
+});
