6b6cf2f25abed39415a4d90708fcb60543709406
Ben Fradet <benjamin.fradet@gmail.com>
2020-04-20 18:12:18 +0200

    Test test watch expression highlighting

--- test/watches.tests.ts
+++ test/watches.tests.ts
@@ -5,12 +5,21 @@ describe("Grammar", () => {
   before(() => { should(); });
 
   describe("Watches", () => {
-    it("expresions", () => {
-      const input = `> this is a watch expression`;
+    it("watch expresion", () => {
+      const input = `> this is a watch`;
       const tokens = tokenize(input)
 
       tokens.should.deep.equal([
         Token.Watches.Watch
+      ]);
+    });
+
+    it("test watch expresion", () => {
+      const input = `test> this is a test watch`;
+      const tokens = tokenize(input)
+
+      tokens.should.deep.equal([
+        Token.Watches.TestWatch
       ]);
     });
   });
